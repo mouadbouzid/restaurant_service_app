11c8c1e855bee5dccbacd06f780c917ad94e9cae
Mouad bzd <mouadbouzidd@gmail.com>
2025-05-01 22:15:59 +0100

    fix error

--- lib/screens/widgets/order_card.dart
+++ lib/screens/widgets/order_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_service/data/models/food_with_quantity_model.dart';
 
-class OrderCard extends StatelessWidget {
+class OrderCard extends StatefulWidget {
   const OrderCard({
     super.key,
     required this.foodWithQuantityModel,
@@ -10,67 +10,66 @@ class OrderCard extends StatelessWidget {
   final FoodWithQuantityModel foodWithQuantityModel;
 
   @override
+  State<OrderCard> createState() => _OrderCardState();
+}
+
+class _OrderCardState extends State<OrderCard> {
+  @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         leading: Image.network(
-          foodWithQuantityModel.foodModel.image,
+          widget.foodWithQuantityModel.foodModel.image,
           width: 60,
           height: 60,
         ),
         title: Text(
-          foodWithQuantityModel.foodModel.name,
+          widget.foodWithQuantityModel.foodModel.name,
           style: TextStyle(fontSize: 18),
         ),
-        subtitle: Text(" ${foodWithQuantityModel.totalPrice()} Dh"),
-        trailing: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-              height: 40,
-              width: 110,
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(40),
+        subtitle: Text(" ${widget.foodWithQuantityModel.totalPrice()} Dh"),
+        trailing: Container(
+          height: 40,
+          width: 120,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(40),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  widget.foodWithQuantityModel.qauntity += 1;
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.add_circle_rounded,
+                  color: Colors.white,
+                  size: 20,
+                ),
               ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      foodWithQuantityModel.qauntity += 1;
-                      print(foodWithQuantityModel.qauntity);
-                    },
-                    icon: Icon(
-                      Icons.add_circle_rounded,
-                      color: Colors.white,
-                      size: 20,
-                    ),
-                  ),
-                  Text(
-                    "${foodWithQuantityModel.qauntity}",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      if (foodWithQuantityModel.qauntity == 0) {
-                      } else {
-                        foodWithQuantityModel.qauntity -= 1;
-                      }
-                      print(foodWithQuantityModel.qauntity);
-                    },
-                    icon: Icon(
-                      Icons.remove_circle,
-                      color: Colors.white,
-                      size: 20,
-                    ),
-                  ),
-                ],
+              Text(
+                "${widget.foodWithQuantityModel.qauntity}",
+                style: TextStyle(color: Colors.white, fontSize: 20),
               ),
-            ),
-          ],
+              IconButton(
+                onPressed: () {
+                  if (widget.foodWithQuantityModel.qauntity == 0) {
+                  } else {
+                    widget.foodWithQuantityModel.qauntity -= 1;
+                    setState(() {});
+                  }
+                },
+                icon: Icon(
+                  Icons.remove_circle,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
