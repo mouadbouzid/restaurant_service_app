9ee254469b98d5ed710ee21c720e16fda4d2d52e
Mouad bzd <mouadbouzidd@gmail.com>
2025-05-11 23:29:59 +0100

    add Textformfields in 'add prodact' creen

--- lib/screens/add_prodact.dart
+++ lib/screens/add_prodact.dart
@@ -1,4 +1,8 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:restaurant_service/data/models/food_category_enum.dart';
+import 'package:restaurant_service/screens/widgets/title_input.dart';
 
 class AddProdact extends StatefulWidget {
   const AddProdact({super.key});
@@ -8,7 +12,17 @@ class AddProdact extends StatefulWidget {
 }
 
 class _AddProdactState extends State<AddProdact> {
+  TextEditingController imageController = TextEditingController();
   TextEditingController nameController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    imageController;
+    nameController;
+    priceController;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,44 +30,64 @@ class _AddProdactState extends State<AddProdact> {
       appBar: AppBar(
         title: Text("Ajauter un produit"),
       ),
-      body: Container(
+      body: Padding(
         padding: EdgeInsets.only(right: 14, left: 14, top: 20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: EdgeInsets.only(top: 1, left: 1),
-              child: Text(
-                'Le nome',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
+            TitleInput(
+                controller: imageController,
+                title: "L'image",
+                hintText: "enter URL d'image",
+                inputType: TextInputType.text),
+            TitleInput(
+                controller: nameController,
+                title: "Le nome",
+                hintText: "enter le nome",
+                inputType: TextInputType.text),
+            TitleInput(
+                controller: priceController,
+                title: "Le prixe",
+                hintText: "enter le prixe",
+                inputType: TextInputType.text),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Catogry :',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
               ),
             ),
             SizedBox(
               height: 10,
             ),
-            TextFormField(
-              controller: nameController,
-              cursorColor: Colors.blueGrey,
+            DropdownButtonFormField2(
               decoration: InputDecoration(
-                hintText: "enter Le nome",
-                hintStyle: const TextStyle(
-                  height: 1,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  borderSide: BorderSide(
-                      color: Colors.grey, width: 1.0), // same as enabled
+                isDense: true,
+                contentPadding: EdgeInsets.zero,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
               ),
-            )
+              buttonStyleData: const ButtonStyleData(
+                height: 50, // <-- change this to your desired field height
+                padding: EdgeInsets.symmetric(horizontal: 16),
+              ),
+              isExpanded: true, // FULL WIDTH
+              hint: Text('Select Category'),
+              items: FoodCategory.values
+                  .map((category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name),
+                      ))
+                  .toList(),
+              onChanged: (value) {
+                // handle selection
+              },
+            ),
           ],
         ),
       ),
---
+++ lib/screens/widgets/title_input.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class TitleInput extends StatelessWidget {
+  const TitleInput({
+    super.key,
+    required this.controller,
+    required this.title,
+    required this.hintText,
+    required this.inputType,
+  });
+
+  final TextEditingController controller;
+  final String title;
+  final String hintText;
+  final TextInputType inputType;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            controller: controller,
+            cursorColor: Colors.blueGrey,
+            decoration: InputDecoration(
+              hintText: hintText,
+              hintStyle: const TextStyle(
+                height: 1,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                borderSide: BorderSide(color: Colors.grey, width: 1.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                borderSide: BorderSide(
+                    color: Colors.grey, width: 1.0), // same as enabled
+              ),
+            ),
+          )
+        ],
+      );
+  }
+}
